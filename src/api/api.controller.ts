b02f7c5aa0bee1a78af2d78d1ef9383625149134
in-seo <sis000512@gmail.com>
2024-08-27 18:39:14 +0900

    feat: parsing another chain's receiver Address
    Signed-off-by: in-seo <sis000512@gmail.com>

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -2,18 +2,47 @@ import { Controller, Get, Query } from "@nestjs/common";
 import { ApiService } from "./api.service";
 import { EtherscanProvider } from "ethers";
 import { ConfigService } from "@nestjs/config";
+import { AxiosError } from "axios";
+import { createClient } from '@layerzerolabs/scan-client';
+import { catchError, firstValueFrom } from "rxjs";
+import { HttpService } from "@nestjs/axios";
 
 
 @Controller('/api')
 export class ApiController {
-  private sepoliaProvider = new EtherscanProvider('sepolia', this.configService.get('SEPOLIA_API_KEY'));
-  private bnbProvider = new EtherscanProvider('bnbt', this.configService.get('BNBSCAN_API_KEY'));
+  private mainnetProvider = new EtherscanProvider('mainnet', this.configService.get('MAINNET_API_KEY'));
+  private bnbProvider = new EtherscanProvider('bnb', this.configService.get('BNBSCAN_API_KEY'));
   constructor(private readonly apiService: ApiService,
-              private readonly configService: ConfigService) {}
+              private readonly configService: ConfigService,
+              private readonly httpService: HttpService) {}
 
-  @Get('/status/sepolia')
-  async getStatusSepolia(@Query('txHash') txHash: string) {
-    return await this.sepoliaProvider.getTransactionReceipt(txHash);
+  @Get('')
+  async getDestinationTxHash() {
+    const client = createClient('mainnet');
+    const {messages} = await client.getMessagesBySrcTxHash(
+      '0x6940134968dc588150608e93f38eb08825488c1b6cbe8dcffe1e63df7c0fd791',
+    );
+
+    const destinationHash = messages.at(0).dstTxHash;
+    const destinationTx = await this.bnbProvider.getTransactionReceipt(destinationHash);
+    const url = `https://api.bscscan.com/api?module=account&action=txlistinternal&txhash=${destinationTx.hash}&apikey=${this.configService.get('BNBSCAN_API_KEY')}`;
+    const { data } = await firstValueFrom(
+      this.httpService.get(url).pipe(
+        catchError((error: AxiosError) => {
+          console.log('Error fetching transaction history:', error.message);
+          throw new Error('An error occurred while fetching transaction history.');
+        }),
+      ),
+    );
+    if(data.message == 'OK') {
+      const receiverAddress = data.result[data.result.length - 1].to;
+      console.log(receiverAddress);
+    }
+  }
+
+  @Get('/status/mainnet')
+  async getStatusMainNet(@Query('txHash') txHash: string) {
+    return await this.mainnetProvider.getTransactionReceipt(txHash);
   }
 
   @Get('/status/bnb')
