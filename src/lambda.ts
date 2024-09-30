8d6eee1981ca01f59808e44cb149df40f709420f
in-seo <sis000512@gmail.com>
2024-09-30 11:57:39 +0900

    deploy: http API Gateway
    Signed-off-by: in-seo <sis000512@gmail.com>

--- src/lambda.ts
+++ src/lambda.ts
@@ -11,7 +11,22 @@ let server: Handler;
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create<NestExpressApplication>(ApiModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-
+  app.enableCors({
+    origin: true,//여기에 url을 넣어도된다.
+    credentials: true,
+    maxAge: 86400,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'X-HTTP-Method-Override',
+      'X-Forwarded-Proto',
+      'X-Forwarded-For',
+      'X-Forwarded-Port'
+    ],
+    optionsSuccessStatus: 204
+  });
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
