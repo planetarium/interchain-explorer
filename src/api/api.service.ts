5cb2428d385f6449af4ae3dd5103a05d0c088720
Yoshi <yoshi@planetariumhq.com>
2025-01-21 11:33:39 +0900

    minor: mixed import cheerio error

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -7,7 +7,7 @@ import { MethodMapperService } from "../common/method-mapper.service";
 import { EventDictionary } from "../common/event.dictionary";
 import { ETHEREUM_API_KEY, BNBSCAN_API_KEY, INFURA_API_KEY, ARBITRUM_API_KEY, BASE_API_KEY} from "../constants/environment";
 import axios from 'axios';
-import * as cheerio from 'cheerio';
+import cheerio from 'cheerio';
 import { CCTPapiError,LayerZeroError } from "../common/errorType";
 import { USDC_ADDRESSES_MAP } from "../common/usdc-address";
 
